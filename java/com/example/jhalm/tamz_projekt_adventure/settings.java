5da59a0852c00ec17f85154391853cf7e539ba9c
halmous <j.halman@seznam.cz>
2018-11-08 12:19:32 +0100

    Menu activity

---
+++ java/com/example/jhalm/tamz_projekt_adventure/settings.java
@@ -0,0 +1,13 @@
+package com.example.jhalm.tamz_projekt_adventure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class settings extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
